bf185095d04b8df2180765f441a5078efd0cb121
davidmcdougal <davidbyu6@gmail.com>
2024-10-16 16:52:35 -0600

    completed work for comments

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -8,12 +8,14 @@ import { useEffect, useRef, useState } from "react";
 import { Input } from "./ui/input";
 import Image from "next/image";
 import { updateDocument } from "@/lib/actions/room.actions";
+import Loader from "./Loader";
 
 const CollaborativeRoom = ({
   roomId,
   roomMetadata,
+  users,
+  currentUserType,
 }: CollaborativeRoomProps) => {
-  const currentUserType = "editor";
   const [editing, setEditing] = useState(false);
   const [loading, setLoading] = useState(false);
   const [documentTitle, setDocumentTitle] = useState(roomMetadata.title);
@@ -64,7 +66,7 @@ const CollaborativeRoom = ({
   }, [editing]);
   return (
     <RoomProvider id={roomId}>
-      <ClientSideSuspense fallback={<div>Loading…</div>}>
+      <ClientSideSuspense fallback={<Loader />}>
         <div className="collaborative-room">
           <Header>
             <div
@@ -112,7 +114,7 @@ const CollaborativeRoom = ({
               </SignedIn>
             </div>
           </Header>
-          <Editor />
+          <Editor roomId={roomId} currentUserType={currentUserType} />
         </div>
       </ClientSideSuspense>
     </RoomProvider>
--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -68,3 +68,12 @@ export const updateDocument = async (roomId: string, title: string) => {
     console.log(`Error happened while updating the room: ${error}`);
   }
 };
+
+export const getDocuments = async (email: string) => {
+  try {
+    const rooms = await liveblocks.getRooms({ userId: email });
+    return parseStringify(rooms);
+  } catch (error) {
+    console.log(`Error happened while trying to access the rooms: ${error}`);
+  }
+};
