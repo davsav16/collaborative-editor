aca8a84d4f5ab3785d01c55161f71d4e000900c6
davidmcdougal <davidbyu6@gmail.com>
2024-10-21 17:03:27 -0600

    created share module

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,9 +1,11 @@
 "use client";
 import Loader from "@/components/Loader";
-import { getClerkUsers } from "@/lib/actions/user.actions";
+import { getClerkUsers, getDocumentUsers } from "@/lib/actions/user.actions";
 import { LiveblocksProvider, ClientSideSuspense } from "@liveblocks/react";
+import { useUser } from "@clerk/nextjs";
 
 const Provider = ({ children }: { children: React.ReactNode }) => {
+  const { user: clerkUser } = useUser();
   return (
     <LiveblocksProvider
       authEndpoint="/api/liveblocks-auth"
@@ -11,6 +13,15 @@ const Provider = ({ children }: { children: React.ReactNode }) => {
         const users = await getClerkUsers({ userIds });
 
         return users;
+      }}
+      resolveMentionSuggestions={async ({ text, roomId }) => {
+        const roomUsers = await getDocumentUsers({
+          roomId,
+          // eslint-disable-next-line @typescript-eslint/no-non-null-asserted-optional-chain
+          currentUser: clerkUser?.emailAddresses[0].emailAddress!,
+          text,
+        });
+        return roomUsers;
       }}
     >
       <ClientSideSuspense fallback={<Loader />}>{children}</ClientSideSuspense>
